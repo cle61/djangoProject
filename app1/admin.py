5c6585d601199e28e061e80ae1e1090cca4c0ce3
Cle61 <prodhomme.clement@hotmail.fr>
2015-12-15 15:24:53 +0100

    add Cat & Cat_color model

--- app1/admin.py
+++ app1/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Cat, Cat_color
+
+admin.site.register(Cat)
+admin.site.register(Cat_color)
--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Cat_color(models.Model):
+    cat_color = models.CharField(max_length=200)
+
+class Cat(models.Model):
+    cat_name = models.CharField(max_length=200)
+    cat_color = models.ForeignKey(Cat_color, on_delete=models.CASCADE)
+
+
